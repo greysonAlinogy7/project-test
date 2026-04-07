16ebfb4f3e78b8861854adefd8c5b2a41768e352
greyson <greysonshawa281@gmail.com>
2026-04-07 17:43:39 +0300

    firt commit

---
+++ src/test/java/com/Tn316/CicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tn316;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
